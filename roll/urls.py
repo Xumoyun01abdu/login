6555d457e2871ca0db298da98ee6e67b97dcc491
Your Name <you@example.com>
2025-10-01 18:37:36 +0500

    kod to'g'irlandi

--- roll/urls.py
+++ roll/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import index, login_user
+from .views import index, login_user, logout_user
 
 
 urlpatterns = [
     path('', index, name='index'),
     path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
